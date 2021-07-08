22ab1a91b80c91d16802fd4cfe3b2870872b210c
Ishraq Haider Chowdhury <ishraq-haider.chowdhury@stud.uni-bamberg.de>
2021-07-08 10:55:27 +0200

    made some changes in the index.js file

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 #!/usr/bin/env node
-const program = require('commander')
+const commander = require('commander')
 const chalk = require('chalk')
 const clipboardy = require('clipboardy')
 const createPassword = require('./utils/createPassword')
 const savePassword = require('./utils/savePassword')
 
-program.version('1.0.0').description('Simple Password Generator')
+commander.version('1.0.0').description('Simple Password Generator')
 
-program
+commander
 .option('-l, --length <number>', 'length of password', '8')
 .option('-s, --save', 'save password to passwords.txt')
 .option('-nn, --no-numbers', 'remove numbers')
 .option('-ns, --no-symbols', 'remove symbols')
 .parse()
 
-const { length, save, numbers, symbols } = program.opts()
+const { length, save, numbers, symbols } = commander.opts()
 
 // Generate password
 const generatedPassword = createPassword(length, numbers, symbols)
